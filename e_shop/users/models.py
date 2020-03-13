36f780cd44f2d273140b73952c60148a0fffb971
LazavikouArtsiom <lazovikovartyom@mail.ru>
2020-03-13 18:11:40 +0300

    netta fix is_stuff

--- e_shop/users/models.py
+++ e_shop/users/models.py
@@ -21,7 +21,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
+    def create_staffruser(self, email, password):
         user = self.create_user(
             email,
             password=password,
